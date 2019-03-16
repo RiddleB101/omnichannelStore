cee0d0f0fc25a95de8a0d47a53ea47f6ec63acde
Huang Jifan <huangjifan1819@163.com>
2019-03-16 12:51:14 +0800

    完成cart页面

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,7 +5,11 @@ Page({
      * 页面的初始数据
      */
     data: {
-        
+        isCart: false,
+        cart: [],
+        total: 0,
+        goodsCount: 0, // 总商品数量
+        selectAllStatus: true
     },
 
     /**
@@ -26,14 +30,33 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () {
-
+        // var that = this;
+        var arr = wx.getStorageSync('cart') || [];
+        if (arr.length > 0) {
+            for (var i in arr) {
+                this.data.total += Number(arr[i].price) * Number(arr[i].count);
+                this.data.goodsCount += Number(arr[i].count);
+                arr[i].selected = true;
+            }
+            this.setData({
+                isCart: true,
+                cart: arr,
+                total: this.data.total,
+                goodsCount: this.data.goodsCount
+            })
+        }
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function () {
-
+        this.setData({
+            isCart: false,
+            cart: [],
+            total: 0,
+            goodsCount: 0
+        });
     },
 
     /**
@@ -62,5 +85,121 @@ Page({
      */
     onShareAppMessage: function () {
 
+    },
+
+    // 本地缓存, 页面之间交互
+
+    /**
+     * 当前商品选中事件
+     */
+    selectList(e) {
+        const index = parseInt(e.currentTarget.dataset.index);
+        let cart = this.data.cart;
+        const selected = cart[index].selected;
+        cart[index].selected = !selected;
+        this.setData({
+            cart: cart
+        });
+        this.getTotalPrice();
+    },
+
+    /**
+     * 删除购物车当前商品
+     */
+    deleteList(e) {
+        const index = parseInt(e.currentTarget.dataset.index);
+        let cart = this.data.cart;
+        cart.splice(index, 1);
+        this.setData({
+            cart: cart
+        });
+        if (!cart.length) {
+            this.setData({
+                isCart: false
+            });
+            try {
+                wx.setStorageSync('cart', []);
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
+            this.getTotalPrice();
+        }
+    },
+
+    /**
+     * 购物车全选
+     */
+    selectAll(e) {
+        let selectAllStatus = this.data.selectAllStatus;
+        selectAllStatus = !selectAllStatus;
+        let cart = this.data.cart;
+
+        for (let i = 0; i < cart.length; i++) {
+            cart[i].selected = selectAllStatus;
+        }
+
+        this.setData({
+            selectAllStatus: selectAllStatus,
+            cart: cart
+        });
+        this.getTotalPrice();
+    },
+
+    /**
+     * 绑定增加事件
+     */
+    addCount(e) {
+        const index = parseInt(e.currentTarget.dataset.index);
+        let cart = this.data.cart;
+        let count = cart[index].count;
+        let goodsCount = cart.goodsCount;
+        goodsCount = goodsCount + 1;
+        cart[index].count = count + 1;
+        this.setData({
+            cart: cart
+        });
+        this.getTotalPrice();
+    },
+
+    /**
+     * 绑定减少事件
+     */
+    minusCount(e) {
+        const index = parseInt(e.currentTarget.dataset.index);
+        let cart = this.data.cart;
+        let count = cart[index].count;
+        let goodsCount = cart.goodsCount;
+        if (count <= 1) {
+            return false;
+        }
+        goodsCount = goodsCount - 1;
+        cart[index].count = count - 1;
+        this.setData({
+            cart: cart
+        });
+        this.getTotalPrice();
+    },
+
+    /**
+     * 计算总价
+     */
+    getTotalPrice() {
+        let cart = this.data.cart;
+        let total = 0;
+        for (let i = 0; i < cart.length; i++) {
+            if (cart[i].selected) {
+                total += cart[i].count * cart[i].price;
+            }
+        }
+        this.setData({
+            cart: cart,
+            total: total.toFixed(2)
+        });
+        try {
+            wx.setStorageSync('cart', this.data.cart);
+        } catch (e) {
+            console.log(e);
+        }
     }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,67 +3,102 @@
 const app = getApp()
 
 Page({
+    // 初始化数据
     data: {
-        array: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        imgUrls: [
+        navImgUrls: [
             'http://img5.imgtn.bdimg.com/it/u=2906541843,1492984080&fm=23&gp=0.jpg',
             'http://img5.imgtn.bdimg.com/it/u=2906541843,1492984080&fm=23&gp=0.jpg',
             'http://img5.imgtn.bdimg.com/it/u=2906541843,1492984080&fm=23&gp=0.jpg',
             'http://img5.imgtn.bdimg.com/it/u=2906541843,1492984080&fm=23&gp=0.jpg',
             'http://img5.imgtn.bdimg.com/it/u=2906541843,1492984080&fm=23&gp=0.jpg'
         ],
+        // 本地初始化数据, 后期提供服务器接口
         goodsList: [{
                 id: "001",
                 imgUrl: "http://img5.imgtn.bdimg.com/it/u=2906541843,1492984080&fm=23&gp=0.jpg",
                 name: "女装T恤中长款大码摆裙春夏新款",
-                price: "65.00"
+                price: 65
             },
             {
                 id: "002",
                 imgUrl: "http://img4.imgtn.bdimg.com/it/u=1004404590,1607956492&fm=23&gp=0.jpg",
                 name: "火亮春秋季 男青年修身款圆领男士T恤",
-                price: "68.00"
+                price: 68
             },
             {
                 id: "003",
                 imgUrl: "http://img1.imgtn.bdimg.com/it/u=2305064940,3470659889&fm=23&gp=0.jpg",
                 name: "新款立体挂脖t恤女短袖大码宽松条纹V领上衣显瘦休闲春夏",
-                price: "86.00"
+                price: 86
             },
             {
                 id: "004",
                 imgUrl: "http://img4.imgtn.bdimg.com/it/u=3986819380,1610061022&fm=23&gp=0.jpg",
                 name: "男运动上衣春季上新品 上衣流行装青年",
-                price: "119.00"
+                price: 119
             },
             {
                 id: "005",
                 imgUrl: "http://img1.imgtn.bdimg.com/it/u=3583238552,3525141111&fm=23&gp=0.jpg",
-                name: "时尚字母三角露胸t恤女装亮丝大码宽松不规则春夏潮",
-                price: "69.00"
+                name: "时尚字母三角t恤女装亮丝大码宽松不规则春夏潮",
+                price: 69
             },
             {
                 id: "006",
                 imgUrl: "http://img2.imgtn.bdimg.com/it/u=1167272381,3361826143&fm=23&gp=0.jpg",
                 name: "新款立体挂脖t恤短袖大码宽松条纹V领上衣显瘦休闲春夏",
-                price: "86.00"
+                price: 86
             },
             {
                 id: "007",
                 imgUrl: "http://img0.imgtn.bdimg.com/it/u=789486313,2033571593&fm=23&gp=0.jpg",
-                name: "时尚字母三角露胸t恤女装亮丝大码宽松不规则春夏潮",
+                name: "时尚字母三角t恤女装亮丝大码宽松不规则春夏潮",
                 price: "119.00"
             },
             {
                 id: "008",
                 imgUrl: "http://img2.imgtn.bdimg.com/it/u=3314044863,3966877419&fm=23&gp=0.jpg",
                 name: "男运动上衣春季上新品 上衣流行装青年",
-                price: "69.00"
+                price: 69
             }
         ]
 
     },
-    //事件处理函数
+
+    // addCart函数
+    addCart: function (e) {
+        this.setData({
+            toastHidden: false
+        });
+        for (var i in this.data.goodsList) {
+            if (this.data.goodsList[i].id == e.target.id) {
+                this.data.goodsList[i].count = 1;
+                var arr = wx.getStorageSync('cart') || [];
+                if (arr.length > 0) {
+                    for (var j in arr) {
+                        if (arr[j].id == e.target.id) {
+                            arr[j].count++;
+                            try {
+                                wx.setStorageSync('cart', arr);
+                            } catch (e) {
+                                console.log(e);
+                            }
+                            return;
+                        }
+                    }
+                    arr.push(this.data.goodsList[i]);
+                } else {
+                    arr.push(this.data.goodsList[i]);
+                }
+                try {
+                    wx.setStorageSync('cart', arr);
+                    return;
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        }
+    },
 
     onLoad: function () {
 
